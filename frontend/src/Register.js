93d36769ba006ccd8e2509c5dfde672a95b49cf2
Stephen McAleese <stephenmcaleese11@gmail.com>
2020-05-29 16:29:40 +0100

    added frontend: simple login and signup version 1.0

---
+++ frontend/src/Register.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: ""
+    };
+  }
+  render() {
+    <div>
+      <AppBar
+        title="Signup"
+      />
+
+    </div>
+  }
+}
